cc945c66a9074d7da6c6541278fed30c066f9c83
raj_prad <pradyuishere@gmail.com>
2018-06-25 19:37:59 +0530

    message01

---
+++ verilog_codes/fifo.v
@@ -0,0 +1,68 @@
+
+module syn_fifo
+   #(
+      parameter DATA_WIDTH = 8,
+      parameter LOG2_DEPTH = 8 // i.e. fifo depth=2**LOG2_DEPTH
+   )
+   (
+      input [DATA_WIDTH-1:0]  data_in,
+      input wr_en,
+      input rd_en,
+      output reg [DATA_WIDTH-1:0] data_out,
+      output full,
+      output empty,
+      input clk,
+      input reset
+   );
+ 
+   parameter MAX_COUNT = 2**LOG2_DEPTH;
+   reg   [LOG2_DEPTH-1 : 0]   rd_ptr;
+   reg   [LOG2_DEPTH-1 : 0]   wr_ptr;
+   reg   [DATA_WIDTH-1 : 0]   mem[MAX_COUNT-1 : 0];   //memory size: 2**LOG2_DEPTH
+   reg   [LOG2_DEPTH : 0]     depth_cnt;
+ 
+   always @(posedge clk) begin
+      if(reset) begin
+         wr_ptr <= 'h0;
+         rd_ptr <= 'h0;
+      end // end if
+      else begin
+         if(wr_en)begin
+            wr_ptr <= wr_ptr+1;
+         end
+         if(rd_en)
+            rd_ptr <= rd_ptr+1;
+      end //end else
+   end//end always
+ 
+   assign empty= (depth_cnt=='h0);
+   assign full = (depth_cnt==MAX_COUNT);
+ 
+   //comment if you want a registered data_out
+   //assign data_out = rd_en ? mem[rd_ptr]:'h0;
+ 
+   always @(posedge clk) begin
+      if (wr_en)
+         mem[wr_ptr] <= data_in;
+   end //end always
+ 
+   //uncomment if you want a registered data_out
+   always @(posedge clk) begin
+      if (reset)
+         data_out <= 'h0;
+      else if (rd_en)
+         data_out <= mem[rd_ptr];
+   end
+ 
+   always @(posedge clk) begin
+      if (reset)
+         depth_cnt <= 'h0;
+      else begin
+         case({rd_en,wr_en})
+            2'b10    :  depth_cnt <= depth_cnt-1;
+            2'b01    :  depth_cnt <= depth_cnt+1;
+         endcase
+      end //end else
+   end //end always
+ 
+endmodule
---
+++ verilog_codes/mac.v
@@ -0,0 +1,39 @@
+
+`define DATA_SIZE 8
+`define MAC_WIDTH 256
+
+module mac_unit(
+	clock,
+	reset,
+	win,		//Stored weight
+	win_request, //Read next value of win
+	instr,
+	data_south1, //data flowing through south
+	data_north1, //data flowing through north
+	data_east1,  //data flowing through east
+	data_west1,   //data flowing through west
+	data_south2, //data flowing through south
+	data_north2, //data flowing through north
+	data_east2,  //data flowing through east
+	data_west2   //data flowing through west
+	);
+//------------------Inputs-------------------
+input clock;
+input reset;
+input win;
+input instr;
+//------------------Outputs------------------
+output win_request;
+//------------------Inout--------------------
+inout [(`DATA_SIZE-1):0] data_south1;
+inout [(`DATA_SIZE-1):0] data_north1;
+inout [(`DATA_SIZE-1):0] data_west1;
+inout [(`DATA_SIZE-1):0] data_east1;
+inout [(`DATA_SIZE-1):0] data_south2;
+inout [(`DATA_SIZE-1):0] data_north2;
+inout [(`DATA_SIZE-1):0] data_west2;
+inout [(`DATA_SIZE-1):0] data_east2;
+//------------------Variables------------------
+integer counter;
+
+endmodule // mac_unit
